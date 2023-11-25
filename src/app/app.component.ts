c9ca25f4cefa5358da7f48ef177b68ea5f3bf245
Abraham Vallejos <abraham-rat@outlook.es>
2023-11-25 04:06:24 -0500

    [Abraham]:implement angular material.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,44 @@
-import { Component, inject, TemplateRef } from '@angular/core';
+import {Component, inject, TemplateRef, ViewChild} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatTableModule} from "@angular/material/table";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
+export interface TableItem {
+  column1: string;
+  column2: string;
+  column3: string;
+  column4: string;
+  column5: string;
+  column6: string;
+}
+const ELEMENT_DATA: TableItem[] = [
+  { column1: 'Data 1', column2: 'Data 2', column3: 'Data 3', column4: 'Data 4', column5: 'Data 5', column6: 'Data 6' },
+  { column1: 'Data 1', column2: 'Data 2', column3: 'Data 3', column4: 'Data 4', column5: 'Data 5', column6: 'Data 6' },
+  { column1: 'Data 1', column2: 'Data 2', column3: 'Data 3', column4: 'Data 4', column5: 'Data 5', column6: 'Data 6' },
+  { column1: 'Data 1', column2: 'Data 2', column3: 'Data 3', column4: 'Data 4', column5: 'Data 5', column6: 'Data 6' },
+  { column1: 'Data 1', column2: 'Data 2', column3: 'Data 3', column4: 'Data 4', column5: 'Data 5', column6: 'Data 6' },
+  // ... Agrega más filas según sea necesario
+];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatFormFieldModule, MatInputModule, MatTabsModule, MatTableModule, MatIconModule, MatButtonModule, MatPaginatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  displayedColumns: string[] = ['column1', 'column2', 'column3', 'column4', 'column5', 'column6', 'column7'];
+  dataSource = ELEMENT_DATA;
   title = 'front-code-challenge';
   private modalService = inject(NgbModal);
   closeResult = '';
@@ -25,6 +53,19 @@ export class AppComponent {
       },
     );
   }
+  pageSize = 5;
+
+  // Página predeterminada
+  pageIndex = 0;
+
+  // ...
+
+  // Función para manejar eventos de paginación
+  handlePage(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.pageIndex = event.pageIndex;
+    // Puedes cargar los datos correspondientes a la nueva página aquí
+  }
 
   private getDismissReason(reason: any): string {
     switch (reason) {
@@ -35,5 +76,6 @@ export class AppComponent {
       default:
         return `with: ${reason}`;
     }
+
   }
 }
